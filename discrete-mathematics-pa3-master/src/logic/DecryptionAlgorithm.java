ff33f523a6a4c95411ab0c9519a4566cd0cacd3f
Michael <michael.tsang@hva.nl>
2020-10-25 16:36:05 +0100

    encrypt and decrypt fixed

--- discrete-mathematics-pa3-master/src/logic/DecryptionAlgorithm.java
+++ discrete-mathematics-pa3-master/src/logic/DecryptionAlgorithm.java
@@ -4,48 +4,35 @@ import java.math.BigInteger;
 
 public class DecryptionAlgorithm {
 
-
-     // This function retrieves the d from the n and e values. It does so by calculating the phi and modular inverse of
-     // e and phi.
-
-     /**
-     * @param n - The product of p & q.
-     * @param e - The public key.
-     * @return returns the d.
-     */
+     //Generate the d value, based on n and e
     public static String generateD(int n, int e) {
         int d = Helper.calculateD(n, e);
         return "d is " + d;
     }
 
-    /**
-     * This function retrieves all the unicode integers of the message.
-     *
-     * @param n - The product of p and q.
-     * @param e - The public key.
-     * @param decryptedMessage - The decrypted message.
-     * @return an array of all the unicode integers.
-     */
+    //Decrypt the message
     public static String generateM(int n, int e, String decryptedMessage){
+
+        //Calculate d
         int d = Helper.calculateD(n, e);
 
+        //Put the words in the array
         String[] result = decryptedMessage.split("\\s*,\\s*");
-
         BigInteger[] c = new BigInteger[result.length];
 
-        // Parse all the values of the public key to an integer.
+        //For loop to parse the values to int
         for (int i = 0; i < result.length; i++) {
             c[i] = BigInteger.valueOf(Integer.parseInt(result[i]));
         }
 
-        int[] m = Helper.calculateM(c,d, n);
+        //Calculate m
+        int[] m = Helper.calculateM(c,d,n);
         char[] mChars = new char[m.length];
 
-        // Parse the integer to a character.
+        //For loop to parse the integer to character.
         for (int i = 0; i < m.length; i++) {
             mChars[i] = Character.toChars(m[i])[0];
         }
-
         return new String(mChars);
     }
 }
--- discrete-mathematics-pa3-master/src/logic/EncryptionAlgorithm.java
+++ discrete-mathematics-pa3-master/src/logic/EncryptionAlgorithm.java
@@ -1,80 +1,44 @@
 package logic;
 
-import controllers.Controller;
-
 import java.math.BigInteger;
 import java.util.Arrays;
-import java.util.Random;
 import java.util.ArrayList;
 
 public class EncryptionAlgorithm {
 
-    /**
-     * This function get the result of p and q, if not p and q can be found show an error message.
-     *
-     * @param n - The prime product of p and q.
-     * @return returns and String with a formatted result.
-     */
+    //Get the p & q based on n
     public static String getPQ(int n) {
         ArrayList<Double> results = Helper.calculatePQ(n);
 
-        // Check if there are any results found.
-        return results.size() == 0 ?
-                "No result found." :
-                "p is " + ((int) results.get(0).doubleValue()) + "\n" +
-                        "q is " + ((int) results.get(1).doubleValue()) + "\n" +
-                        "Amount of time busy finding p and q: " + results.get(2);
+        return "p is " + ((int) results.get(0).doubleValue()) + "\n" +
+                "q is " + ((int) results.get(1).doubleValue()) + "\n" +
+                "Amount of time busy finding p and q: " + results.get(2) + " milliseconds";
     }
 
-    /**
-     * Get random prime number
-     *
-     * @param n - The prime product of p and q.
-     * @return prime number e
-     */
+    //Get random prime number
     public static String getRandomE(int n) {
         return Helper.getRandomE(n);
     }
 
-    public static Boolean verifyE(String n, String e){
-        ArrayList<Integer>  results = Helper.generatePrimeNumbers(Integer.parseInt(n));
-        if (results.contains(Integer.parseInt(e))) {
-            return true;
-        }else{
-            return false;
-        }
-    }
+    //Generate encrypted message
+    public static String generateC(String n, String message, int e){
+        char[] characters = message.toCharArray();
+        int[] mNumberInt = new int[message.length()];
 
-    /**
-     * This function will encrypt a message from the user input
-     * @param n key
-     * @param message message to encrypt
-     * @return the encrypted message
-     */
-    public static String generateC(String n, String message, int userE){
-        //Check if user submitted his own e
-        int e;
-        if (verifyE(n, Integer.toString(userE))){
-            e = userE;
-        }else{
-            e = Integer.parseInt(Helper.getRandomE(Integer.parseInt(n)));
+        //For loop to parse character to int
+        for (int m = 0; m < message.length(); m++) {
+            mNumberInt[m] = characters[m];
         }
 
-        char[] chars = message.toCharArray();
-        int[] mNumbersInt = new int[message.length()];
+        BigInteger mNumber[] = new BigInteger[message.length()];
 
-        // Parse character to int
-        for (int j = 0; j < message.length(); j++) {
-            mNumbersInt[j] = chars[j];
+        //For loop for the message numbers
+        for (int j = 0; j < message.length(); j++){
+            mNumber[j] = BigInteger.valueOf(mNumberInt[j]);
         }
 
-        BigInteger mNumbers[] = new BigInteger[message.length()];
-
-        for (int i = 0; i < message.length(); i++){
-            mNumbers[i] = BigInteger.valueOf(mNumbersInt[i]);
-        }
-
-        int[] encryptedArray = Helper.calculateC(mNumbers,e, Integer.parseInt(n));
+        //Generate the encrypted message
+        int[] encryptedArray = Helper.calculateC(mNumber, e, Integer.parseInt(n));
 
         return "e is: " + e + "\n" +
                 "Message after encrypting is: " + Arrays.toString(encryptedArray);
