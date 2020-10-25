b6b0ddfabee23f7ff44d4760abcd313164a0b0c1
Michael <michael.tsang@hva.nl>
2020-10-25 15:23:01 +0100

    a

--- discrete-mathematics-pa3-master/src/controllers/Controller.java
+++ discrete-mathematics-pa3-master/src/controllers/Controller.java
@@ -1,7 +1,6 @@
 package controllers;
 
 import javafx.fxml.FXML;
-import javafx.scene.control.CheckBox;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 import logic.DecryptionAlgorithm;
@@ -9,21 +8,15 @@ import logic.EncryptionAlgorithm;
 
 public class Controller {
 
-    /* Encrypt fields */
+    //Encrypt fields
     @FXML
-    TextField encryptInputN;
+    TextField inputN;
     @FXML
-    TextField encryptInputM;
+    TextField inputM;
     @FXML
-    CheckBox checkBoxMultipleE;
-    @FXML
-    TextField encryptInputE;
-    @FXML
-    CheckBox checkBoxUserE;
-    @FXML
-    TextArea encryptOutput;
+    TextArea output;
 
-    /* Decrypt fields */
+    //Decrypt fields
     @FXML
     TextField decryptInputN;
     @FXML
@@ -33,69 +26,45 @@ public class Controller {
     @FXML
     TextArea decryptOutput;
 
-    final String cssDefault = "-fx-border-color: gray;";
-    final String cssError = "-fx-border-color: red;";
-
     /**
      * Encrypt functionality.
      */
     public void encryptButtonStep1Clicked() {
-        String inputN = encryptInputN.getText();
-
+        String inputN = this.inputN.getText();
         // Validate if there is an input.
         if (inputN.equals("")) return;
-
         String output = EncryptionAlgorithm.getPQ(Integer.parseInt(inputN));
-
-        encryptOutput.setText(output);
+        this.output.setText(output);
     }
 
     /**
      * Encrypt functionality.
      */
     public void encryptButtonStep2Clicked() {
-        String inputN = encryptInputN.getText();
+        String inputN = this.inputN.getText();
 
         // Validate if there is an input.
         if (inputN.equals("")) return;
 
         StringBuilder e = new StringBuilder();
 
-        if (checkBoxMultipleE.isSelected()){
-            e.append("e's are: ");
-            for (int i = 0; i < 5; i++){
-                e.append(EncryptionAlgorithm.getRandomE(Integer.parseInt(inputN))).append(", ");
-            }
-        }else{
-            e.append("e is: ");
-            e.append(EncryptionAlgorithm.getRandomE(Integer.parseInt(inputN)));
-        }
+        e.append("e is: ");
+        e.append(EncryptionAlgorithm.getRandomE(Integer.parseInt(inputN)));
 
-        encryptOutput.setText(e.toString());
+        output.setText(e.toString());
     }
 
     public void encryptButtonStep3Clicked() {
-        String inputN = encryptInputN.getText();
-        String inputM = encryptInputM.getText();
+        String inputN = this.inputN.getText();
+        String inputM = this.inputM.getText();
 
         if (inputN.equals("") || inputM.equals("")) return;
-
         int userE;
-        if (!encryptInputE.equals("") && checkBoxUserE.isSelected()){
-            if (EncryptionAlgorithm.verifyE(inputN, encryptInputE.getText())){
-                userE = Integer.parseInt(encryptInputE.getText());
-                encryptInputE.setStyle(cssDefault);
-            }else{
-                userE = 0;
-                encryptInputE.setStyle(cssError);
-            }
-        }else{
-            userE = 0;
-        }
+        userE = 0;
 
         String output = EncryptionAlgorithm.getC(inputN, inputM, userE);
 
-        encryptOutput.setText(output);
+        this.output.setText(output);
     }
 
     /**
@@ -130,12 +99,7 @@ public class Controller {
     /**
      * onaction event for enabling e field for userE
      */
-    public void checkBoxEnable(){
-        if (checkBoxUserE.isSelected()){
-            encryptInputE.setDisable(false);
-        }else{
-            encryptInputE.setDisable(true);
-        }
-    }
 
 }
+
+
